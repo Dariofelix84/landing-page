6a5f8948e7c0c1e033a46d513a87618e04528baa
Dariofelix84 <dariofa69@gmail.com>
2024-06-13 15:51:51 -0300

    initial commit

---
+++ script.js
@@ -0,0 +1,32 @@
+const wrapper = document.querySelector('.wrapper');
+const loginLink = document.querySelector('.login-link');
+const registrarLink = document.querySelector('.registrar-link');
+const btnPopup = document.querySelector('.btnLogin-popup');
+const iconClose = document.querySelector('.icon-close');
+const sidebar = document.querySelector(".sidebar");
+const hamburger = document.querySelector(".hamburger");
+const headerWrapper = document.querySelector(".header-wrapper");
+
+hamburger.addEventListener("click", ()=> {
+    headerWrapper.classList.toggle("show-menu")
+    sidebar.classList.toggle("sidebar-show")
+});
+
+registrarLink.addEventListener('click', ()=> {
+    wrapper.classList.add('active')   
+});
+
+loginLink.addEventListener('click', ()=> {
+    wrapper.classList.remove('active')
+});
+
+btnPopup.addEventListener('click', ()=> {
+    wrapper.classList.add('active-popup');
+    sidebar.classList.remove("sidebar-show")
+    headerWrapper.classList.remove("show-menu")
+});
+
+iconClose.addEventListener('click', ()=> {
+    wrapper.classList.remove('active-popup');
+});
+
